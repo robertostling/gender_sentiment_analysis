85123f0512224189b26e1a499c27939034b19ade
Robert Östling <robert@ling.su.se>
2023-09-27 15:26:30 +0200

    Generation of tables for analysis

--- progs/histwords/analyze.py
+++ progs/histwords/analyze.py
@@ -2,22 +2,21 @@ import sys
 import pickle
 from pprint import pprint
 import os.path
-
-from matplotlib import pyplot as plt
-from cycler import cycler
+import csv
 
 import numpy as np
-import pinyin
 
-# skiv:/home/corpora/histwords/chi-sim-all/sgns
+# Argument: a directory where histwords zip files have been unpacked
+histwords_path = sys.argv[1]
+
 # https://github.com/wac81/Chinese-Sentiment/tree/master/Sentiment%20features/Sentiment%20dictionary%20features/sentiment%20dictionary%20source
 # https://github.com/shekhargulati/sentiment-analysis-python/tree/master/opinion-lexicon-English
 
-def load_sentiment_vectors(path, vocab_idx, m):
-    with open(os.path.join(path, 'negative.txt')) as f:
+def load_sentiment_vectors(path_pattern, vocab_idx, m):
+    with open(path_pattern.format('negative')) as f:
         negative_words = f.read().split()
 
-    with open(os.path.join(path, 'positive.txt')) as f:
+    with open(path_pattern.format('positive')) as f:
         positive_words = f.read().split()
 
     positive_vocab = [w for w in positive_words if w in vocab_idx]
@@ -38,22 +37,27 @@ def load_embeddings(path_prefix):
     return vocab, m
 
 
-def estimate_trajectories(sentiment_path, histwords_path, years, target_words):
+def estimate_trajectories(sentiment_path_pattern, vectors_path, years,
+                          target_words):
     result = {}
     for year in years:
-        path_prefix = f'{histwords_path}/{year}'
+        path_prefix = f'{vectors_path}/{year}'
         vocab, m = load_embeddings(path_prefix)
         vocab_idx = {w:i for i,w in enumerate(vocab)}
         negative_vocab, negative, positive_vocab, positive \
-                = load_sentiment_vectors(sentiment_path, vocab_idx, m)
+                = load_sentiment_vectors(sentiment_path_pattern, vocab_idx, m)
 
         print(f'Creating {year} model from {len(negative)} negative and '
               f'{len(positive)} positive items...')
 
-        positivity = np.mean(positive, axis=0) - np.mean(negative, axis=0)
+        positive_centroid = np.mean(positive, axis=0)
+        negative_centroid = np.mean(negative, axis=0)
+        positivity = positive_centroid - negative_centroid
+        centroid = 0.5 * (positive_centroid + negative_centroid)
 
         def project(v):
-            return np.dot(v, positivity) / np.dot(positivity, positivity)
+            return np.dot(v - centroid, positivity) \
+                    / np.dot(positivity, positivity)
 
         target_vocab = [w for w in target_words if w in vocab_idx]
         target_m = np.array([m[vocab_idx[w]] for w in target_vocab])
@@ -65,49 +69,126 @@ def estimate_trajectories(sentiment_path, histwords_path, years, target_words):
     return result
 
 
-def do_chinese(histwords_path):
-    target_words = ('妻子 丈夫 母亲 父亲 女儿 '
-                    '儿子 好人 坏人').split()
+TERM_LISTS = {
+        'chinese': [
+            ('adult', 'f', '女人', None),
+            ('adult', 'm', '男人', None),
+            ('not_adult', 'f', '女孩子 女孩儿 女孩', None),
+            ('not_adult', 'm', '男孩子 男孩儿 男孩', None),
+            ('parent', 'f', '母亲 妈妈', None),
+            ('parent', 'm', '父亲 爸爸', None),
+            ('child', 'f', '女儿', None),
+            ('child', 'm', '儿子', None),
+            ('sibling', 'f', '姐姐 妹妹 姐妹', None),
+            ('sibling', 'm', '哥哥 弟弟 兄弟', None),
+            ('spouse', 'f', '妻子', None),
+            ('spouse', 'm', '丈夫', None),
+            ('parent_in_law', 'f', '婆婆 岳母', None),
+            ('parent_in_law', 'm', '公公 岳父', None),
+             ],
+        'english': [
+            ('adult', 'f','woman', 's'),
+            ('adult', 'f', 'women', 'p'),
+            ('adult', 'm', 'man', 's'),
+            ('adult', 'm', 'men', 'p'),
+            ('not_adult', 'f', 'girl', 's'),
+            ('not_adult', 'f', 'girls', 'p'),
+            ('not_adult', 'm', 'boy', 's'),
+            ('not_adult', 'm', 'boys', 'p'),
+            ('parent', 'f', 'mother', 's'),
+            ('parent', 'f', 'mothers', 'p'),
+            ('parent', 'm', 'father', 's'),
+            ('parent', 'm', 'fathers', 'p'),
+            ('child', 'f', 'daughter', 's'),
+            ('child', 'f', 'daughters', 'p'),
+            ('child', 'm', 'son', 's'),
+            ('child', 'm', 'sons', 'p'),
+            ('sibling', 'f', 'sister', 's'),
+            ('sibling', 'f', 'sisters', 'p'),
+            ('sibling', 'm', 'brother', 's'),
+            ('sibling', 'm', 'brothers', 'p'),
+            ('spouse', 'f', 'wife', 's'),
+            ('spouse', 'f', 'wives', 'p'),
+            ('spouse', 'm', 'husband', 's'),
+            ('spouse', 'm', 'husbands', 'p'),
+            ('parent_in_law', 'f', 'mother-in-law', 's'),
+            ('parent_in_law', 'f', 'mothers-in-law', 'p'),
+            ('parent_in_law', 'm', 'father-in-law', 's'),
+            ('parent_in_law', 'm', 'fathers-in-law', 'p'),
+            ]}
+
+
+def generate_table(filename, language, sentiment_path_pattern, vectors_path,
+                   identifier, from_year, to_year):
+    terms = TERM_LISTS[language]
+    words = {form for _, _, forms, _ in terms
+                  for form in forms.split()}
+    years = list(range(from_year, to_year+10, 10))
     result = estimate_trajectories(
-            'chinese', os.path.join(histwords_path, 'chi-sim-all', 'sgns'),
-            [str(year) for year in range(1950, 2000, 10)], target_words)
-    return result
+            sentiment_path_pattern, vectors_path, list(map(str, years)), words)
+    with open(filename, 'w', newline='') as f:
+        writer = csv.writer(f, delimiter='\t', quotechar='"',
+                            quoting=csv.QUOTE_MINIMAL)
+        writer.writerow('model language concept gender form number year value'.split())
+        for concept, gender, forms, number in terms:
+            for form in forms.split():
+                for year in years:
+                    if form not in result[year]:
+                        continue
+                    value = result[year][form]
+                    number = 'x' if number is None else number
+                    writer.writerow([identifier, language, concept, gender,
+                                     form, number, year, value])
 
-def do_english(histwords_path):
-    target_words = (#'woman man '
-                    'wife husband mother father daughter son angel devil '
-                    #'women men wives husbands mothers fathres daughters sons '
-                    #'good bad horrible terrible awesome fantastic great awful '
-                    #'kind caring selfish worthless '
-                    ).split()
-    result = estimate_trajectories(
-            'english', os.path.join(histwords_path, 'eng-fiction-all', 'sgns'),
-            [str(year) for year in range(1900, 2000, 10)], target_words)
-    return result
+
+generate_table('english-greenwald-fiction.tsv', 'english',
+               'english/{}-greenwald.txt',
+               os.path.join(histwords_path, 'eng-fiction-all', 'sgns'),
+               'fiction_greenwald', 1900, 1990)
+
+generate_table('english-greenwald-coha.tsv', 'english',
+               'english/{}-greenwald.txt',
+               os.path.join(histwords_path, 'coha-word', 'sgns'),
+               'coha_greenwald', 1900, 1990)
+
+generate_table('english-huliu-coha.tsv', 'english',
+               'english/{}-words.txt',
+               os.path.join(histwords_path, 'coha-word', 'sgns'),
+               'coha_huliu', 1900, 1990)
+
+generate_table('chinese-ntu.tsv', 'chinese', 'chinese/ntusd-{}.txt',
+               os.path.join(histwords_path, 'chi-sim-all', 'sgns'),
+               'ntu', 1950, 1990)
 
 
-def plot_result(result, filename=None):
-    default_cycler = (
-            cycler(color=['r', 'g', 'b', 'y', 'c', 'm', 'y', 'k']) +
-            cycler(linestyle=['-', '--', ':', '-.', '-', '--', ':', '-.']))
-    plt.rc('axes', prop_cycle=default_cycler)
-
-    vocab = {word for sentiments in result.values()
-                  for word in sentiments.keys()}
-    for word in vocab:
-        xs = sorted(result.keys())
-        ys = [result[x][word] for x in xs]
-        plt.plot(xs, ys, label=pinyin.get(word))
-    plt.legend()
-    if filename:
-        plt.savefig(filename)
-    plt.show()
-
-if False:
-    result = do_chinese(sys.argv[1])
-    plot_result(result, 'chinese.pdf')
-else:
-    result = do_english(sys.argv[1])
-    plot_result(result, 'english.pdf')
-pprint(result)
+# Plotting code, expecting the output from estimate_trajectories:
+#
+#import pinyin
+#from matplotlib import pyplot as plt
+#from cycler import cycler
+#
+#def plot_result(result, filename=None):
+#    default_cycler = (
+#            cycler(color=['r', 'g', 'b', 'y', 'c', 'm', 'y', 'k']) +
+#            cycler(linestyle=['-', '--', ':', '-.', '-', '--', ':', '-.']))
+#    plt.rc('axes', prop_cycle=default_cycler)
+#
+#    vocab = {word for sentiments in result.values()
+#                  for word in sentiments.keys()}
+#    for word in vocab:
+#        xs = sorted(result.keys())
+#        ys = [result[x][word] for x in xs]
+#        plt.plot(xs, ys, label=pinyin.get(word))
+#    plt.legend()
+#    if filename:
+#        plt.savefig(filename)
+#    plt.show()
+#
+#if False:
+#    result = do_chinese(sys.argv[1])
+#    plot_result(result, 'chinese.pdf')
+#else:
+#    result = do_english(sys.argv[1])
+#    plot_result(result, 'english.pdf')
+#pprint(result)
 
